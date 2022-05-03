26081ffe1315a9364dceffd9a7d0cfc536d05060
mariuslmaooo <104183684+mariuslmaooo@users.noreply.github.com>
2022-05-02 21:06:57 -0700

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F660GNWT54"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){window.dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'GA_MEASUREMENT_ID');
+  gtag('config', 'G-F660GNWT54');
 </script>
