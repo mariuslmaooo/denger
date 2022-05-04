42024dc33ca33dd74893c00a067b068f07b8ace7
Denger <95730536+dengertheguy@users.noreply.github.com>
2022-05-03 17:40:34 -0700

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F660GNWT54"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){window.dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-F660GNWT54');
-</script>
